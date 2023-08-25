f631b7fbbd66fb98647e75284e5d6dccaf76bf35
lfro2 <lfro2@cin.ufpe.br>
2023-08-24 23:35:35 -0300

    Alteração no codigo da api

--- back/apiChat.py
+++ back/apiChat.py
@@ -2,22 +2,27 @@ from senhaChat import API_KEY
 import requests
 import json
 
-# Definir o nome do produto
-produto = "microondas"
+def apiChatGpt(textoEntrada):
+    headears = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
+    link = "https://api.openai.com/v1/chat/completions"
+    id_modelo = "gpt-3.5-turbo"
 
-headears = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
-link = "https://api.openai.com/v1/chat/completions"
-id_modelo = "gpt-3.5-turbo"
+    body_mensagem = {
+        "model": id_modelo,
+        "messages": [{"role": "user", "content": textoEntrada}]
+    }
 
-textoEntrada = "crie 5 frases de efeito, sendo pesuasivos, para vender " + produto
+    body_mensagem = json.dumps(body_mensagem)
 
-body_mensagem = {
-    "model": id_modelo,
-    "messages": [{"role": "user", "content": textoEntrada}]
-}
+    requisicao = requests.post(link, headears=headears, data=body_mensagem)
+    reposta = requisicao.json
+    mensagem = resposta["choices"][0]["message"]["content"]
 
-body_mensagem = json.dumps(body_mensagem)
+    return mensagem
 
-requisicao = requests.post(link, headears=headears, data=body_mensagem)
-reposta = requisicao.json
-mensagem = resposta["choices"][0]["message"]["content"]
+
+texto = "crie 5 frases de efeito curtas para vender microondas"
+
+textoSaida =  apiChatGpt(texto)
+
+print(textoSaida)
--- back/apiClip.py
+++ back/apiClip.py
@@ -1,29 +1,32 @@
 from senhaClip import API_KEY
 import requests
 
-# Link da API
-link = "https://clipdrop-api.co/remove-background/v1"
-# Nome do arquivo (é necessário que o arquivo esteja dentro da pasta imagensEntrada)
-nomeImagemArquivo = "pessoa.jpg" 
-pathImagem = "imagensEntrada/" + nomeImagemArquivo  
+def apiRemoveBackGround(nomeArquivo):
+    # Link da API
+    link = "https://clipdrop-api.co/remove-background/v1"
+    pathImagem = "imagensEntrada/" + nomeArquivo  
 
-# Vai abrir a imagem e colocar na variável image_data
-with open(pathImagem, 'rb') as image_file:
-    image_data = image_file.read()
+    # Vai abrir a imagem e colocar na variável image_data
+    with open(pathImagem, 'rb') as image_file:
+        image_data = image_file.read()
 
-# Comunicação com a API
-r = requests.post(link,
-  files = {
-    'image_file': ('teste.jpg', image_data, 'image/jpeg'),
-    },
-  headers = { 'x-api-key': API_KEY}
-)
+    # Comunicação com a API
+    r = requests.post(link,
+    files = {
+        'image_file': ('teste.jpg', image_data, 'image/jpeg'),
+        },
+    headers = { 'x-api-key': API_KEY}
+    )
 
-# Caso ocorra tudo certo, a imagem será salva na pasta imagensSaida
-if r.ok:
-    pathSaida = "imagensSaida/" + nomeImagemArquivo
-    with open(pathSaida, 'wb') as output_file:
-        output_file.write(r.content)
-    print("Imagem de saída salva com sucesso!")
-else:
-    r.raise_for_status()
+    # Caso ocorra tudo certo, a imagem será salva na pasta imagensSaida
+    if r.ok:
+        pathSaida = "imagensSaida/" + nomeArquivo
+        with open(pathSaida, 'wb') as output_file:
+            output_file.write(r.content)
+        print("Imagem de saída salva com sucesso!")
+    else:
+        r.raise_for_status()
+
+
+nomeArquivoImagem = "pessoa.jpg"
+apiRemoveBackGround(nomeArquivoImagem)
