94cdfeeff6b1c6e4766ad111d7cf8ec10ff07af8
lfro2 <lfro2@cin.ufpe.br>
2023-08-24 22:33:51 -0300

    Alteração da apiChatGpt

--- back/apiChat.py
+++ back/apiChat.py
@@ -2,11 +2,13 @@ from senhaChat import API_KEY
 import requests
 import json
 
+# Definir o nome do produto
+produto = "microondas"
+
 headears = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
 link = "https://api.openai.com/v1/chat/completions"
 id_modelo = "gpt-3.5-turbo"
 
-produto = "microondas"
 textoEntrada = "crie 5 frases de efeito, sendo pesuasivos, para vender " + produto
 
 body_mensagem = {
