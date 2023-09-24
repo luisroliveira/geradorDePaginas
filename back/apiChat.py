a7cdc1df15c65e84b194b2b389f28e8c08260fb4
Matheus Paixão <mpgs@cin.ufpe.br>
2023-09-24 13:04:55 -0300

    Adição de prompts para gerar especificamente frase, texto e slogan

--- back/apiChat.py
+++ back/apiChat.py
@@ -59,18 +59,108 @@ def apiChatGpt(textoEntrada):
     return json_msg
 
 def gerarFrase(textoEntrada):
+    # headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
+    # link = "https://api.openai.com/v1/chat/completions"
+    # id_modelo = "gpt-4"
+
+    # body_mensagem = {
+    #     "model": id_modelo,
+    #     "messages": [
+    #         {
+    #             "role": "system",
+    #             "content": """Você é um ótimo vendedor, alguém que conhece muito sobre publicidade e que consegue convencer qualquer pessoa a realizar uma compra. Vou te dar o nome do produto, o que é o produto e uma breve descrição, nessa ordem. Seu objetivo é gerar 4 frases persuasivas que eu possa colocar em uma landing page de vendas. Mantenha as frases concisas. 
+    #             Responda no seguinte template JSON:
+    #             {
+    #             "result": ["Frase  1"]
+    #             }
+    #             """
+    #         },
+    #         {
+    #             "role": "user", 
+    #             "content": textoEntrada
+    #         }
+    #     ]
+    # }
+
+    # body_mensagem = json.dumps(body_mensagem)
+
+    # requisicao = requests.post(link, headers=headers, data=body_mensagem)
+    # resposta = requisicao.json()
+    # mensagem = resposta["choices"][0]["message"]["content"]
+    # return mensagem
+
     json_response = {
         "result": ["Com o barber+, fazer a barba nunca foi tão fácil e confortável.", "Experimente a sensação suave e sem irritações que só nosso barber+ pode oferecer.", "Não perca mais tempo! Com o barber+, obtenha um corte rápido e preciso para se manter sempre bem aparentado.", "Desfrute de uma experiência de barbear luxuosa com o nosso inovador barber+."]
     }
     return json_response
 
 def gerarTexto(textoEntrada):
+    # headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
+    # link = "https://api.openai.com/v1/chat/completions"
+    # id_modelo = "gpt-4"
+
+    # body_mensagem = {
+    #     "model": id_modelo,
+    #     "messages": [
+    #         {
+    #             "role": "system",
+    #             "content": """Você é um ótimo vendedor, alguém que conhece muito sobre publicidade e que consegue convencer qualquer pessoa a realizar uma compra. Vou te dar o nome do produto, o que é o produto e uma breve descrição, nessa ordem. Seu objetivo é gerar 2 textos persuasivos que eu possa colocar em uma landing page de vendas. Cada texto deve possuir 1 parágrafo e ser feito para retratar a situação de uso do produto e sua proposta de valor. Além disso, não comece o texto pedindo para imaginar algo. 
+    #             Responda no seguinte template JSON:
+    #             {
+    #             "result": ["Texto  1"]
+    #             }
+    #             """
+    #         },
+    #         {
+    #             "role": "user", 
+    #             "content": textoEntrada
+    #         }
+    #     ]
+    # }
+
+    # body_mensagem = json.dumps(body_mensagem)
+
+    # requisicao = requests.post(link, headers=headers, data=body_mensagem)
+    # resposta = requisicao.json()
+    # mensagem = resposta["choices"][0]["message"]["content"]
+    # return mensagem
+
     json_response = {
         "result": ["1Não há nada como começar o dia com uma cara fresca e limpa, é por isso que apresentamos o Barber+. Este barbeador premium não é apenas um produto, mas sim um investimento em sua aparência pessoal. Fácil de usar, rápido no corte e suave na pele, você sentirá a diferença desde o primeiro uso. Com o Barber+ você estará sempre pronto para encarar seu dia da melhor maneira possível!", "1O Barber+ redefine o ato de fazer a barba. Diga adeus às lâminas antigas que puxam e irritam a pele. Nosso barbeador dá-lhe um corte suave e perfeito todas as vezes, tornando sua rotina matinal mais rápida e menos dolorosa. Agora você pode ter uma barba bem feita e sem esforço, deixando mais tempo para as coisas que realmente importam."]
     }
     return json_response
 
 def gerarSlogan(textoEntrada):
+    # headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type":"application/json"}
+    # link = "https://api.openai.com/v1/chat/completions"
+    # id_modelo = "gpt-4"
+
+    # body_mensagem = {
+    #     "model": id_modelo,
+    #     "messages": [
+    #         {
+    #             "role": "system",
+    #             "content": """Você é um ótimo vendedor, alguém que conhece muito sobre publicidade e que consegue convencer qualquer pessoa a realizar uma compra. Vou te dar o nome do produto, o que é o produto e uma breve descrição, nessa ordem. Seu objetivo é gerar 4 slogans que eu possa colocar em uma landing page de vendas. Os slogans devem ser feitos de forma bem criativa. 
+    #             Responda no seguinte template JSON:
+    #             {
+    #             "result": ["Slogan  1"]
+    #             }
+    #             """
+    #         },
+    #         {
+    #             "role": "user", 
+    #             "content": textoEntrada
+    #         }
+    #     ]
+    # }
+
+    # body_mensagem = json.dumps(body_mensagem)
+
+    # requisicao = requests.post(link, headers=headers, data=body_mensagem)
+    # resposta = requisicao.json()
+    # mensagem = resposta["choices"][0]["message"]["content"]
+    # return mensagem
+
     json_response = {
         "result": ["1Barber+: Sua melhor imagem começa aqui.", "1Alcance a perfeição em cada corte com Barber+.", "1Barber+, elevando sua experiência de barbear ao máximo."]
     }
