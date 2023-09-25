bf19c5afad944a1150754e60a86c62fcbfc3eeba
Matheus Paixão <mpgs@cin.ufpe.br>
2023-09-25 20:46:47 -0300

    Adicionando mais permissões

--- back/servidor.py
+++ back/servidor.py
@@ -9,7 +9,9 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 api_v1_cors_config = {
-    "origins": ["*"]
+    "origins": ["*"],
+    "methods": ["OPTIONS", "GET", "POST", "DELETE", "PUT"],
+    "allow_headers": ["Authorization", "Content-Type"]
 }
 
 CORS(app, resources={
